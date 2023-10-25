a00a915b4f2264985f53ff8fbe2ae568cf34be6a
Murilo Matioli <96125707+Matiolilegalzao@users.noreply.github.com>
2023-10-25 12:31:05 -0300

    primeiro

---
+++ 03 Eventos DOM/script.js
@@ -0,0 +1,14 @@
+function clicar(){
+    var area = window.document.getElementById('area')
+    area.innerHTML = 'Clicou'
+}
+
+function entrar(){
+    var area = window.document.getElementById('area')
+    area.innerHTML = 'Mouse detectado!'
+}
+
+function sair(){
+    var area = window.document.getElementById('area')
+    area.innerHTML = 'Mouse não-detectado'
+}
