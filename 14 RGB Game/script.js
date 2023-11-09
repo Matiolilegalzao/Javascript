34b88d4b26838749d3759f3c1378dea227b644b2
Murilo Lima Matioli <96125707+Matiolilegalzao@users.noreply.github.com>
2023-11-09 15:43:32 -0400

    Função de randomizar cores adicionada

--- 14 RGB Game/script.js
+++ 14 RGB Game/script.js
@@ -1,3 +1,30 @@
-let botao = [6];
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function clickButton(buttonNumber){ //função que retorna qual o botão que apertei
+  if(buttonNumber == correctButton){
+    alert("acertou")
+  }else {
+    alert("errou")
+  }
+}
+function newColor(){
+  
+  correctButton = randomNumber(1, 6); //escolhe qual botão será o correto
+  console.log(correctButton)
+  document.getElementById("newColor") //atribui newColor() ao li
+  let botões = document.querySelectorAll(".btn"); //cria um vetor com todos os .btn
 
-for (let i = 0; i < 6; i++) {}
+  for (let i = 0; i < botões.length; i++) {
+    let R = randomNumber(0, 255);
+    let G = randomNumber(0, 255);
+    let B = randomNumber(0, 255);
+    botões[i].style.backgroundColor = "rgb(" + R + "," + G + "," + B + ")"; //atribui cor aos botões
+
+    if(i == correctButton){  //quando o contador chega no botão correto a ser escolhido, muda o html para o rgb
+    let rgbcode = document.getElementById("rgbcode");
+        botões[i].style.backgroundColor = "rgb(" + R + "," + G + "," + B + ")";
+    rgbcode.innerHTML = `rgb(${R}, ${G},${B})`;
+    }
+  }
+}
